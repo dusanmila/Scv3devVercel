f8b2cd51f27cf018a74330698042e12a5a1a81dc
stefanradulovicuns <stefanradulovic@uns.ac.rs>
2022-05-03 11:44:38 +0200

    forma za objekte

--- src/app/components/object/object.component.ts
+++ src/app/components/object/object.component.ts
@@ -10,6 +10,59 @@ import { Obj } from 'src/app/services/object.service';
 export class ObjectComponent implements OnInit {
 
   private _objects: Obj[] = [];
+  public object: Obj = {
+    objectIdRetail: "string",
+    objectIdCompany: "string",
+    retailer: "string",
+    objectFormat: "string",
+    objectName: "string",
+    city: "string",
+    address: "string",
+    kam: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+    },
+    director: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+    },
+    supervisor: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+    },
+    commercialist: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+    },
+    merchandiser: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: ""
+    },
+    requisitionDays: "",
+    merchandiserRevisionDays: "string",
+    objectInfo: {
+      assortmentModule: "",
+      gainings12Mrsd: 0,
+      wdpercentSerbia: 0,
+      wdpercentSector: 0,
+      wdpercentCustomer: 0,
+      gainingsVs12mpercent: 0,
+      registersNumber: 0,
+      shelfSpaceM: 0,
+      companyShelfSpaceM: 0,
+      companyShelfSpacePercent: 0
+    }
+  }
 
   public get objects() {
     return this._objects;
@@ -18,10 +71,26 @@ export class ObjectComponent implements OnInit {
   constructor(public objectService: ObjectService) { }
 
   ngOnInit(): void {
-    console.log('aaa');
-    this.objectService.getObjects().subscribe(data => {
-      this._objects = data;
-      console.log(this._objects);
+    // this.objectService.getObjects().subscribe(data => {
+    //   this._objects = data;
+    //   console.log(this._objects);
+    // });
+
+    // this.objectService.getObjectsByString('ste').subscribe(data => {
+    //   this._objects = data;
+    //   console.log(this._objects);
+    // });
+
+    // this.objectService.getObjectByObjectName('Objekat1').subscribe(data => {
+    //   this.object = data;
+    //   console.log(this.object);
+    // });
+  }
+
+  public createObject() {
+    console.log(this.object);
+    this.objectService.createObject(this.object).subscribe(data => {
+      console.log(data);
     });
   }
 
--- src/app/services/object.service.ts
+++ src/app/services/object.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ObjectInfo } from './object-info.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpStatusCode } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 
-export interface Obj{
+export interface User {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+}
+
+export interface Obj {
   objectIdRetail: string;
   objectIdCompany: string;
+  retailer: string;
   objectFormat: string;
   objectName: string;
   city: string;
   address: string;
+  kam: User;
+  director: User;
+  supervisor: User;
+  commercialist: User;
+  merchandiser: User;
   requisitionDays: string;
   merchandiserRevisionDays: string;
   objectInfo: ObjectInfo;
@@ -26,10 +39,41 @@ export class ObjectService {
   private readonly address = "http://localhost:8089/api/objects";
 
   public getObjects(): Observable<Obj[]> {
-    console.log('bbb');
     let retval$ = new Subject<Obj[]>();
     this.http.get<Obj[]>(this.address).subscribe((objects: Obj[]) => {
       retval$.next(objects);
+    });
+    return retval$.asObservable();
+  }
+
+  public getObjectByObjectName(objectName: string): Observable<Obj> {
+    let retval$ = new Subject<Obj>();
+    this.http.get<Obj>(`${this.address}/objectByObjectName/${objectName}`).subscribe((helper: Obj) => {
+      retval$.next(helper);
+    });
+    return retval$.asObservable();
+  }
+
+  public getObjectsByString(str: string): Observable<Obj[]> {
+    let retval$ = new Subject<Obj[]>();
+    this.http.get<Obj[]>(`${this.address}/objectByString/${str}`).subscribe((objects: Obj[]) => {
+      retval$.next(objects);
+    });
+    return retval$.asObservable();
+  }
+
+  public createObject(object: Obj): Observable<Obj> {
+    let retval$ = new Subject<Obj>();
+    this.http.post<Obj>(this.address, object).subscribe((helper: Obj) => {
+      retval$.next(helper);
+    });
+    return retval$.asObservable();
+  }
+
+  public updateObject(object: Obj): Observable<Obj> {
+    let retval$ = new Subject<Obj>();
+    this.http.put<Obj>(this.address, object).subscribe((helper: Obj) => {
+      retval$.next(helper);
     });
     return retval$.asObservable();
   }
