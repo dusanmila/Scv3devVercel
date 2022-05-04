0034e15cdb316cea09511984d53ee4fbb9724bad
Stefan Fink <stefan.fink123@gmail.com>
2022-05-04 17:27:29 +0200

    Useri preko Materiala sa username pretragom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { UserComponent } from './user/user.component';
 import { AdminGuard } from  './admin/admin.guard';
+import { AdminpageComponent } from './adminpage/adminpage.component';
 
 const routes: Routes = [
+  {path:'', redirectTo:'admin',pathMatch:'full'},
   {path:'feedback', component:FeedbackComponent},
-  {path:'user', component:UserComponent}
+  {path:'user', component:UserComponent},
+  {path:'admin', component:AdminpageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PositionComponent } from './position/position.component';
-import { FormsModule } from '@angular/forms';
+
 
 import { ObjectComponent } from './components/object/object.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 import { FeedbackComponent } from './feedback/feedback.component';
 
 import { WebcamModule } from 'ngx-webcam';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -29,15 +34,20 @@ import { LoginComponent } from './login/login.component';
     ObjectComponent,
     FeedbackComponent,
     UserComponent,
-    LoginComponent
+    LoginComponent,
+    AdminpageComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    MatInputModule,
     BrowserAnimationsModule,
-    WebcamModule
+    WebcamModule,
+    BrowserModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
