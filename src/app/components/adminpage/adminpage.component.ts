6fdd9e78e47c2be7cdf29d146cb7e3e19dbf8bc0
Stefan Fink <65467915+finkstefan@users.noreply.github.com>
2022-05-10 15:59:15 +0200

    excel imports poziv servisa

--- src/app/components/adminpage/adminpage.component.ts
+++ src/app/components/adminpage/adminpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ObjectService } from 'src/app/Services/object.service';
+import { PositionService } from 'src/app/Services/position-service.service';
 
 @Component({
   selector: 'app-adminpage',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private objectService:ObjectService,private positionService:PositionService) { }
 
   ngOnInit(): void {
   }
+objectsFile:any;
+positionsFile:any;
+
+uploadObjectsFile(event:any){
+  this.objectsFile=event.target.files[0];
+  console.log(event.target.files[0].name);
+  let formData = new FormData();
+formData.set('file',this.objectsFile);
+
+this.objectService.excelImport(formData);
+}
+
+uploadPositionsFile(event:any){
+  this.positionsFile=event.target.files[0];
+  console.log(event.target.files[0].name);
+  let formData = new FormData();
+  formData.set('file',this.positionsFile);
+
+  this.positionService.excelImport(formData);
+}
+
+
 
 }
