80df0942570e431524d37da0f6756a5f8dbafcaf
DusanMilasinovic <milasinovicdusan@gmail.com>
2022-06-10 14:49:33 +0200

    tacka

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,4 +9,4 @@ export const USER_URL = 'https://microserviceuser.azurewebsites.net/api';
 export const POSITION_URL = 'https://microserviceposition.azurewebsites.net/api';
 export const FEEDBACK_URL = 'https://microservicefeedback20220523153422.azurewebsites.net/api';
 export const OBJECT_URL = 'https://microserviceobject.azurewebsites.net/api';
-export const AUTH_URL = 'https://microserviceauthazurewebsites.net/api';
+export const AUTH_URL = 'https://microserviceauth.azurewebsites.net/api';
