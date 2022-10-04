94617a0e9f8e5486d1f97f0b635cda33bca5ffaa
DusanMilasinovic <milasinovicdusan@gmail.com>
2022-10-04 11:54:45 +0200

    komentariConstants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,26 +1,3 @@
-// export const STORE_CHECK_URL = 'http://localhost:8085/api';
-// export const USER_URL = 'http://localhost:8086/api';
-// export const POSITION_URL = 'http://localhost:8087/api';
-// export const FEEDBACK_URL = 'http://localhost:8088/api';
-// export const OBJECT_URL = 'http://localhost:8089/api';
-// export const AUTH_URL = 'https://localhost:44323/api';
-/*
-export const STORE_CHECK_URL = 'https://microservicestorecheck.azurewebsites.net/api';
-export const USER_URL = 'https://microserviceuser.azurewebsites.net/api';
-export const POSITION_URL = 'https://microserviceposition.azurewebsites.net/api';
-export const FEEDBACK_URL = 'https://microservicefeedback20220523153422.azurewebsites.net/api';
-export const OBJECT_URL = 'https://microserviceobject.azurewebsites.net/api';
-export const AUTH_URL = 'https://microserviceauth.azurewebsites.net/api';
-*/
-/*
-//monolit
-
-export const STORE_CHECK_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const USER_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const POSITION_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const FEEDBACK_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const OBJECT_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const AUTH_URL = 'https://storecheckmonolith.azurewebsites.net/api';*/
 
  export const STORE_CHECK_URL = 'https://storecheckbeta.azurewebsites.net/api';
  export const USER_URL = 'https://storecheckbeta.azurewebsites.net/api';
@@ -28,22 +5,3 @@ export const AUTH_URL = 'https://storecheckmonolith.azurewebsites.net/api';*/
  export const FEEDBACK_URL = 'https://storecheckbeta.azurewebsites.net/api';
  export const OBJECT_URL = 'https://storecheckbeta.azurewebsites.net/api';
  export const AUTH_URL = 'https://storecheckbeta.azurewebsites.net/api';
-
-//azure monolit
-/*
-export const STORE_CHECK_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const USER_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const POSITION_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const FEEDBACK_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const OBJECT_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-export const AUTH_URL = 'https://storecheckmonolith.azurewebsites.net/api';
-*/
-
-// hetzner server
-// export const STORE_CHECK_URL = 'https://storecheck.nortz.rs/api';
-// export const USER_URL = 'https://storecheck.nortz.rs/api';
-// export const POSITION_URL = 'https://storecheck.nortz.rs/api';
-// export const FEEDBACK_URL = 'https://storecheck.nortz.rs/api';
-// export const OBJECT_URL = 'https://storecheck.nortz.rs/api';
-// export const AUTH_URL = 'https://storecheck.nortz.rs/api';
-
