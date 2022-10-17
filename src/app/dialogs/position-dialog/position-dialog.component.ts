06e9464683b505c099cf5f8d3c14e2ad7575238a
Stefan Fink <65467915+finkstefan@users.noreply.github.com>
2022-10-17 12:54:14 +0200

    dimenzije slika pozicija

--- src/app/dialogs/position-dialog/position-dialog.component.ts
+++ src/app/dialogs/position-dialog/position-dialog.component.ts
@@ -8,6 +8,7 @@ import { ProductCategory } from 'src/app/models/productCategory';
 import { PositionService } from 'src/app/Services/position-service.service';
 import { ProductCategoryService } from 'src/app/Services/product-category.service';
 import { Guid } from 'guid-typescript';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-position-dialog',
@@ -16,14 +17,20 @@ import { Guid } from 'guid-typescript';
 })
 export class PositionDialogComponent implements OnInit {
 
+  public form: FormGroup;
   public flag: number;
   public objectIdCompany: string;
   public positionClasses: PositionClass[];
   public positionTypes: PositionType[];
   public productCategories: ProductCategory[];
   public changed: boolean = false;
+  public submitClicked:boolean=false;
+  public imageUploaded:boolean=false;
+  public isLoading:boolean=false;
+  public secPos = { secondaryPositionId: "",  objectIdCompany: "", posClassName: "", posTypeName:"", productCategory: "", supplier:"", location: "",img:"",isImgHorizontal:false, valid:false};
   public suppliers = ['Frikom', 'Other'];
   public locations = ['Magacin', 'Prodajni prostor'];
+  isRotated=false;
 
   positionDto : Position = {
     secondaryPositionId:Guid.create(),
@@ -33,17 +40,29 @@ export class PositionDialogComponent implements OnInit {
     productCategory: "",
     supplier: "",
     location: "",
+    img:"",
+    isImgHorizontal:false,
     valid: false
   };
+
 
   constructor(public snackBar: MatSnackBar,
     public dialogRef: MatDialogRef<PositionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Position,
     public positionService: PositionService,
-    public productCategoryService: ProductCategoryService) { }
+    public productCategoryService: ProductCategoryService,public fb: FormBuilder) {
+      this.form = this.fb.group({
+      file: [null],
+      img: ['']
+    });}
 
   ngOnInit(): void {
-    console.log(this.data)
+
+this.positionDto=this.data;
+
+console.log(this.positionDto)
+console.log(this.positionDto.img)
+
     this.loadPositionClasses();
     this.loadPositionTypes();
     this.loadProductCategories();
@@ -98,13 +117,13 @@ export class PositionDialogComponent implements OnInit {
   }
 
   public update(): void {
-this.positionDto.secondaryPositionId=this.data.secondaryPositionId;
-    this.positionDto.location=this.data.location;
-    this.positionDto.objectIdCompany=this.objectIdCompany;
-    this.positionDto.posClassName=this.data.posClassName;
-    this.positionDto.posTypeName=this.data.posTypeName;
-    this.positionDto.supplier = this.data.supplier;
-    this.positionDto.productCategory=this.data.productCategory;
+   // this.positionDto.secondaryPositionId=this.secPos.secondaryPositionId;
+    this.positionDto.location=this.positionDto.location;
+    this.positionDto.objectIdCompany=this.positionDto.objectIdCompany;
+    this.positionDto.posClassName=this.positionDto.posClassName;
+    this.positionDto.posTypeName=this.positionDto.posTypeName;
+    this.positionDto.supplier = this.positionDto.supplier;
+    this.positionDto.productCategory=this.positionDto.productCategory;
 
     this.positionService.updatePosition(this.positionDto)
       .subscribe(() => {
@@ -117,6 +136,148 @@ this.positionDto.secondaryPositionId=this.data.secondaryPositionId;
         this.close();
       }
   }
+
+  uploadFile(event: any) {
+    const file = (event.target as HTMLInputElement).files![0];
+    this.form.patchValue({
+      file: file,
+    });
+    this.form.get('file')!.updateValueAndValidity();
+    this.imageUploaded = true;
+  }
+
+  submitFormCreate() {
+    this.submitClicked=true;
+    if(this.imageUploaded){
+      this.isLoading=true;
+      let username = localStorage.getItem("username") as string;
+      const formData: any = new FormData();
+      formData.append('file', this.form.get('file')!.value);
+      formData.append('secondaryPositionId', this.positionDto.secondaryPositionId);
+      formData.append('objectIdCompany', this.objectIdCompany);
+      formData.append('posClassName', this.positionDto.posClassName);
+      formData.append('posTypeName', this.positionDto.posTypeName);
+      formData.append('productCategory', this.positionDto.productCategory);
+      formData.append('supplier', this.positionDto.supplier);
+      formData.append('location', this.positionDto.location);
+      formData.append('img', this.positionDto.img);
+      formData.append('valid', this.positionDto.valid);
+
+      this.positionService.createPosition(formData).subscribe(data => {
+        this.changed = true;
+        this.isLoading = false;
+        console.log(data);
+        this.snackBar.open('Position added', 'Ok', { duration: 2500, panelClass: ['blue-snackbar'] });
+
+        this.close();
+      }),
+      (error:Error) => {
+        console.log(error.name + ' -> ' + error.message)
+        this.snackBar.open('An error occurred.', 'Close', { duration: 2500, panelClass: ['red-snackbar'] });
+      };
+    }
+
+  }
+
+  submitFormUpdate() {
+    this.submitClicked=true;
+    if(this.imageUploaded){
+      this.isLoading=true;
+      let username = localStorage.getItem("username") as string;
+      const formData: any = new FormData();
+      formData.append('file', this.form.get('file')!.value);
+      formData.append('secondaryPositionId', this.positionDto.secondaryPositionId);
+      formData.append('objectIdCompany', this.objectIdCompany);
+      formData.append('posClassName', this.positionDto.posClassName);
+      formData.append('posTypeName', this.positionDto.posTypeName);
+      formData.append('productCategory', this.positionDto.productCategory);
+      formData.append('supplier', this.positionDto.supplier);
+      formData.append('location', this.positionDto.location);
+      formData.append('img', this.positionDto.img);
+      formData.append('valid', this.positionDto.valid);
+
+      this.positionService.updatePosition(formData).subscribe(data => {
+        this.changed = true;
+        this.isLoading = false;
+        console.log(data);
+        this.snackBar.open('Position updated', 'Ok', { duration: 2500, panelClass: ['blue-snackbar'] });
+
+        this.close();
+      }),
+      (error:Error) => {
+        console.log(error.name + ' -> ' + error.message)
+        this.snackBar.open('An error occurred.', 'Close', { duration: 2500, panelClass: ['red-snackbar'] });
+      };
+    }
+
+  }
+
+  ngAfterViewInit(): void {
+
+if(this.positionDto.img){
+  this.adjustImage();
+}
+
+
+    }
+
+    async adjustImage(){
+
+
+
+      const img = document.getElementById('posphoto') as HTMLImageElement;
+
+      window.exifr.parse(img!).then((exif) => {
+
+        if (exif.Orientation == 6) {
+        this.isRotated=true;
+        }
+
+console.log("isrotated"+this.isRotated)
+console.log("ishor"+this.data.isImgHorizontal)
+
+        if(this.isRotated){
+
+          if(this.data.isImgHorizontal==true){
+
+            img.setAttribute('height','250');
+            img.setAttribute('width','180');
+
+          }else{
+
+            img.setAttribute('height','200');
+            img.setAttribute('width','250');
+
+
+
+          }
+        }else{
+
+          if(this.data.isImgHorizontal==true){
+
+            img.setAttribute('height','180');
+            img.setAttribute('width','250');
+
+          }else{
+
+            img.setAttribute('height','250');
+            img.setAttribute('width','200');
+
+
+          }
+        }
+
+     if(this.isRotated){
+            img.setAttribute('class','rotate');
+     }
+
+
+      })
+
+
+
+
+    }
 
   public close() {
     this.dialogRef.close(this.changed);
