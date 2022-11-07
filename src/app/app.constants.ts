d46387f70516d20b2d8cac9c94f44fe726564ca5
DusanMilasinovic <milasinovicdusan@gmail.com>
2022-11-07 11:34:08 +0100

    rute

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,13 +1,13 @@
 
-// export const STORE_CHECK_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const USER_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const POSITION_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const FEEDBACK_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const OBJECT_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const AUTH_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const SERVICE_URL = 'https://storecheckbeta.azurewebsites.net/api';
-// export const PRODUCT_URL = 'https://storecheckbeta.azurewebsites.net/api';
-
+ export const STORE_CHECK_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const USER_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const POSITION_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const FEEDBACK_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const OBJECT_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const AUTH_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const SERVICE_URL = 'https://storecheckbeta.azurewebsites.net/api';
+ export const PRODUCT_URL = 'https://storecheckbeta.azurewebsites.net/api';
+/*
 export const STORE_CHECK_URL = 'http://localhost:8081/api';
 export const USER_URL = 'http://localhost:8081/api';
 export const POSITION_URL = 'http://localhost:8081/api';
@@ -15,4 +15,4 @@ export const FEEDBACK_URL = 'http://localhost:8081/api';
 export const OBJECT_URL = 'http://localhost:8081/api';
 export const AUTH_URL = 'http://localhost:8081/api';
 export const SERVICE_URL = 'http://localhost:8081/api';
-export const PRODUCT_URL = 'http://localhost:8081/api';
+export const PRODUCT_URL = 'http://localhost:8081/api';*/
