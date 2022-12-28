bdc075762407c3606d8c21fee8938c0063f573ea
fink <stefan.fink123@gmail.com>
2022-12-28 16:49:22 +0100

    Poruke dodate u are you sure dialog

--- src/app/dialogs/are-you-sure-dialog/are-you-sure-dialog.component.ts
+++ src/app/dialogs/are-you-sure-dialog/are-you-sure-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-are-you-sure-dialog',
@@ -10,13 +10,26 @@ export class AreYouSureDialogComponent   {
 
   public retval: boolean = false;
 
-  constructor(public dialogRef: MatDialogRef<AreYouSureDialogComponent>) { }
+  public message:string="";
 
+  constructor(public dialogRef: MatDialogRef<AreYouSureDialogComponent>,@Inject(MAT_DIALOG_DATA) public data:any) { }
+
+  ngOnInit(): void {
+    console.log(this.data)
+   if(this.data.flag==1){
+this.message="If you delete products, returns will be deleted too."
+   }else if(this.data.flag==2){
+    this.message="If you delete objects, positions will be deleted too."
+   }
+
+  }
  
   public exit() {
     this.retval = true;
     this.close();
   }
+
+  
 
   public close(): void {
     this.dialogRef.close(this.retval);
