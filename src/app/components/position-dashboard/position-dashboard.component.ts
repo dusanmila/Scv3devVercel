0befe3ef3e82d2a47ded5963ca465a5934ff8b03
Duki <milasinovicdusan@gmail.com>
2022-12-28 15:28:09 +0100

    a

---
+++ src/app/components/position-dashboard/position-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-position-dashboard',
+  templateUrl: './position-dashboard.component.html',
+  styleUrls: ['./position-dashboard.component.css']
+})
+export class PositionDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
