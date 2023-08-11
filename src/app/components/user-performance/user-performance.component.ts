5260dc8263ad80b7cadf6ce1ff43c428dbac5fbe
Stefan Radulovic <stefanradulovic@uns.ac.rs>
2023-08-11 12:38:12 +0200

    Prikaz user performance

--- src/app/components/user-performance/user-performance.component.ts
+++ src/app/components/user-performance/user-performance.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { PromoService } from 'src/app/Services/promo.service';
+import { UserPerformance } from 'src/app/models/userPerformance';
 
 @Component({
   selector: 'app-user-performance',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPerformanceComponent implements OnInit {
 
-  constructor() { }
+  userPerformances: UserPerformance[] = [];
+  dataSource: MatTableDataSource<UserPerformance>;
+  displayedColumns: string[] = ['username', 'user', 'avgEstimationSuccess'];
+  isLoading: boolean = false;
+  noData: boolean = false;
+  length: number = 0;
+  search: string = '';
+  page: number = 1;
+  count: number = 5;
+
+  constructor(private promoService: PromoService) { }
 
   ngOnInit(): void {
+    this.loadData(false);
+  }
+
+  loadData(pageChanged: boolean) {
+    if (!pageChanged)
+      this.page = 1;
+    this.isLoading = true;
+    this.promoService.getUserPerformance(this.search, this.page, this.count).subscribe(data => {
+      this.isLoading = true;
+      if (data) {
+        this.dataSource = new MatTableDataSource<UserPerformance>(data);
+        // this.length = data[0].totalCount;
+        this.noData = false;
+        this.length = data[0].totalCount;
+      } else {
+        this.noData = true;
+        this.dataSource = data;
+        this.length = 0
+      }
+      this.isLoading = false;
+    });
+  }
+
+  loadDataOnPageEvent(event: PageEvent) {
+    this.count = event.pageSize;
+    this.page = event.pageIndex + 1;
+    this.loadData(true);
   }
 
 }
