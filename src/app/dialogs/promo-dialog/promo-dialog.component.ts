fcf9e34746ca0e37c770c379d017e349dfd22e6b
Stefan Radulovic <stefanradulovic@uns.ac.rs>
2023-09-28 14:57:28 +0200

    Gp cash u dijalogu

--- src/app/dialogs/promo-dialog/promo-dialog.component.ts
+++ src/app/dialogs/promo-dialog/promo-dialog.component.ts
@@ -50,7 +50,6 @@ export class PromoDialogComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.data);
     this.getRetailers();
     this.getProducts();
     this.getRetailerNames();
